3594a83a3fcc7a2c7bc1b565d362288dac7768a8
Amira Hassan <amirahassansobhi@gmail.com>
2024-09-30 12:50:26 +0300

    Delete code directory

--- code/build/default/debug/MCAL/INTERRUPT/internal_interrupt.p1.d
+++
@@ -1,8 +0,0 @@
-build/default/debug/MCAL/INTERRUPT/internal_interrupt.p1:  \
-MCAL/INTERRUPT/internal_interrupt.c  \
-MCAL/INTERRUPT/internal_interrupt.h  \
-MCAL/INTERRUPT/manage_interrupt.h  \
-MCAL/INTERRUPT/interrupt_config.h  \
-MCAL/INTERRUPT/interrupt_gen_cfg.h  \
-MCAL/INTERRUPT/../GPIO/hal_gpio.h  \
-MCAL/INTERRUPT/../GPIO/../mcal_std_types.h 
--- code/build/default/debug/MCAL/Timer1/mcal_tmr1.p1.d
+++
@@ -1,9 +0,0 @@
-build/default/debug/MCAL/Timer1/mcal_tmr1.p1:  \
-MCAL/Timer1/mcal_tmr1.c  \
-MCAL/Timer1/mcal_tmr1.h  \
-MCAL/Timer1/../GPIO/hal_gpio.h  \
-MCAL/Timer1/../GPIO/../mcal_std_types.h  \
-MCAL/Timer1/../INTERRUPT/internal_interrupt.h  \
-MCAL/Timer1/../INTERRUPT/manage_interrupt.h  \
-MCAL/Timer1/../INTERRUPT/interrupt_config.h  \
-MCAL/Timer1/../INTERRUPT/interrupt_gen_cfg.h 
